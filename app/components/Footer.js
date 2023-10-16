3149f4d763afa58e546aeb0a9415c8640a42546c
JulianoLeperso <118521709+JulianoLeperso@users.noreply.github.com>
2023-10-17 00:23:46 +0200

    changed component file name

---
+++ app/components/Footer.js
@@ -0,0 +1,16 @@
+
+
+
+export default function Header(){
+  return (
+    <footer className="flex flex-1 py-8 justify-center items-center text-black bg-white text-center">
+      <a
+        href="http://www.linkedin.com/in/alexandre-bensarsa-aba267267"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by Us
+      </a>
+    </footer>
+  )
+}
