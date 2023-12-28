11876a2bde0384b9d73071ceada0542ab4168d80
Alexandre Bensarsa <alexandre.bensarsa@jeece.fr>
2023-12-28 02:34:26 +0100

    feat: redirecting to login page from the header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,54 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useUser } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
 
 export default function Header() {
-  const [userProfile, setUserProfile] = useState(null);
+  const user = useUser();
+  const router = useRouter();
 
-  useEffect(() => {
-    async function fetchUserProfile() {
-      try {
-        const response = await fetch('/api/profile');
-        if (response.ok) {
-          const profile = await response.json();
-          setUserProfile(profile);
-        } else if (response.status === 401) {
-          console.log('User is not logged in.');
-        }
-      } catch (error) {
-        console.error('Failed to fetch user profile:', error);
-      }
+  const handleAuthButtonClick = () => {
+    if (user) {
+      router.push('/profile');
+    } else {
+      router.push('/login');
     }
-
-    fetchUserProfile();
-  }, []);
+  };
 
   return (
     <header className="sticky top-0 z-50 bg-white text-black p-8 flex">
       <div className="flex-grow">
-        <Link href={`/`}>
-          <div className="flex items-center space-x-2 cursor-pointer">
-            <Image src="/ECE_LOGO_2021_web.png" alt="ECE Logo" width={150} height={50} />
+        <Link href="/" passHref>
+          <span className="flex items-center space-x-2 cursor-pointer">
+            <Image src="/ECE_LOGO_2021_web.png" alt="ECE Logo" width={150} height={50} priority />
             <span>Web technologies</span>
-          </div>
+          </span>
         </Link>
       </div>
       <ul className="list-none m-0 p-0 inline-flex space-x-8">
         <li>
-          <Link legacyBehavior href="/articles"><a>Articles</a></Link>
+          <Link href="/articles">Articles</Link>
         </li>
         <li>
-          <Link legacyBehavior href="/about"><a>About us</a></Link>
+          <Link href="/about">About us</Link>
         </li>
         <li>
-          <Link legacyBehavior href="/contacts"><a>Contact us</a></Link>
+          <Link href="/contacts">Contact us</Link>
         </li>
-        {userProfile && (
-          <li className="flex items-center space-x-2">
-            <span>👤</span>
-            <span>{userProfile.username}</span>
-          </li>
-        )}
+        <li>
+          <button onClick={handleAuthButtonClick} className="text-sm font-semibold">
+            {user ? user.email : 'Sign in'}
+          </button>
+        </li>
       </ul>
     </header>
   );
