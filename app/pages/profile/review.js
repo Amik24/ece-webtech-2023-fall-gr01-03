56eddbbd5af2bffe859b2590e2dc09b3f6da2c73
Alexandre Bensarsa <alexandre.bensarsa@jeece.fr>
2023-12-29 00:42:14 +0100

    feat: modify comment from user

--- app/pages/profile/review.js
+++ app/pages/profile/review.js
@@ -4,8 +4,11 @@ import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 
 export default function MyReviews() {
   const [reviews, setReviews] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(-1);
+  const [editComment, setEditComment] = useState('');
+  const [editRating, setEditRating] = useState(0);
   const supabase = useSupabaseClient();
-  const user  = useUser(); // This will be used to fetch reviews and display user email
+  const user = useUser(); // Assuming useUser hook provides user details
 
   useEffect(() => {
     if (user) {
@@ -16,7 +19,7 @@ export default function MyReviews() {
   const fetchReviews = async () => {
     const { data: reviewsData, error } = await supabase
       .from('ratings')
-      .select('commentaire, rating, id_film')
+      .select('id, commentaire, rating, id_film')
       .eq('email', user.email);
 
     if (error) {
@@ -24,13 +27,11 @@ export default function MyReviews() {
       return;
     }
 
-    // Fetch movie details for each review
     const movieDetailsPromises = reviewsData.map((review) =>
       fetch(`http://www.omdbapi.com/?i=${review.id_film}&apikey=aadb27a`).then((res) => res.json())
     );
     const moviesDetails = await Promise.all(movieDetailsPromises);
 
-    // Combine movie details with reviews
     const reviewsWithMovies = reviewsData.map((review, index) => ({
       ...review,
       movie: moviesDetails[index],
@@ -39,16 +40,62 @@ export default function MyReviews() {
     setReviews(reviewsWithMovies);
   };
 
+  const handleEdit = (review, index) => {
+    setEditingIndex(index);
+    setEditComment(review.commentaire);
+    setEditRating(review.rating);
+  };
+
+  const handleUpdate = async (reviewId) => {
+    const { error } = await supabase
+      .from('ratings')
+      .update({ commentaire: editComment, rating: editRating })
+      .eq('id', reviewId);
+
+    if (error) {
+      console.error('Error updating review:', error);
+    } else {
+      fetchReviews(); // Re-fetch reviews after updating
+      setEditingIndex(-1); // Exit editing mode
+    }
+  };
+
+  const cancelEdit = () => {
+    setEditingIndex(-1);
+  };
+
   return (
     <Layout>
       <h1>My Reviews</h1>
       <ul>
         {reviews.map((review, index) => (
-          <li key={index}>
-            <h3>{review.movie.Title} ({review.movie.Year})</h3>
-            <img src={review.movie.Poster} alt={`Poster of ${review.movie.Title}`} />
-            <p>Rating: {review.rating}</p>
-            <p>Comment: {review.commentaire}</p>
+          <li key={review.id}>
+            {editingIndex === index ? (
+              <div>
+                <input
+                  type="text"
+                  value={editComment}
+                  onChange={(e) => setEditComment(e.target.value)}
+                />
+                <input
+                  type="number"
+                  min="1"
+                  max="5"
+                  value={editRating}
+                  onChange={(e) => setEditRating(e.target.value)}
+                />
+                <button onClick={() => handleUpdate(review.id)}>Update</button>
+                <button onClick={cancelEdit}>Cancel</button>
+              </div>
+            ) : (
+              <div>
+                <h3>{review.movie.Title} ({review.movie.Year})</h3>
+                <img src={review.movie.Poster} alt={`Poster of ${review.movie.Title}`} />
+                <p>Rating: {review.rating}</p>
+                <p>Comment: {review.commentaire}</p>
+                <button onClick={() => handleEdit(review, index)}>Edit Comment</button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
