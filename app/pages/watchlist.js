8acdaa6678ff205de06ac5390fd0fb569524f179
Alexandre Bensarsa <alexandre.bensarsa@jeece.fr>
2023-12-29 18:10:23 +0100

    feat: deleting comments from watchlist

--- app/pages/watchlist.js
+++ app/pages/watchlist.js
@@ -16,7 +16,7 @@ export default function Watchlist() {
   const fetchWatchlist = async () => {
     const { data: watchlistData, error } = await supabase
       .from('watchlist')
-      .select('id_film')
+      .select('*')
       .eq('email', user.email);
 
     if (error) {
@@ -25,12 +25,33 @@ export default function Watchlist() {
     }
 
     const movieDetailsPromises = watchlistData.map((entry) => {
-      // Use your internal API endpoint to fetch movie details
       return fetch(`/api/omdb?i=${entry.id_film}`).then((res) => res.json());
     });
 
     const moviesDetails = await Promise.all(movieDetailsPromises);
-    setWatchlist(moviesDetails);
+    // Combine the movie details with the id_film from your watchlist data
+    const combinedData = moviesDetails.map((details, index) => ({
+      ...details,
+      id_film: watchlistData[index].id_film,
+    }));
+    setWatchlist(combinedData);
+  };
+
+  const handleDelete = async (id_film) => {
+    const { error } = await supabase
+      .from('watchlist')
+      .delete()
+      .eq('id_film', id_film)
+      .eq('email', user.email);
+
+    if (error) {
+      console.error('Error deleting movie from watchlist:', error);
+    } else {
+      // Remove the movie from the local state as well
+      const updatedWatchlist = watchlist.filter(movie => movie.id_film !== id_film);
+      setWatchlist(updatedWatchlist);
+      alert('Movie removed from watchlist');
+    }
   };
 
   return (
@@ -39,14 +60,20 @@ export default function Watchlist() {
       <div className="container mx-auto p-4">
         {watchlist.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {watchlist.map((movie, index) => (
-              <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg">
+            {watchlist.map((movie) => (
+              <div key={movie.id_film} className="max-w-sm rounded overflow-hidden shadow-lg">
                 <img src={movie.Poster} alt={`Poster of ${movie.Title}`} className="w-full" />
                 <div className="px-6 py-4">
-                <p><strong>Release Date:</strong> {movie.Released}</p>
-                <p><strong>Synopsis:</strong> {movie.Plot}</p>
-                <p><strong>Length:</strong> {movie.Runtime}</p>
-                <p><strong>Genres:</strong> {movie.Genre}</p>
+                  <p><strong>Release Date:</strong> {movie.Released}</p>
+                  <p><strong>Synopsis:</strong> {movie.Plot}</p>
+                  <p><strong>Length:</strong> {movie.Runtime}</p>
+                  <p><strong>Genres:</strong> {movie.Genre}</p>
+                  <button 
+                    onClick={() => handleDelete(movie.id_film)}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
             ))}
